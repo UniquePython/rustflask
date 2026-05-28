53623f10d09bc669ec15ce6f4f9773aa7f2fd9d1
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-05-28 20:00:27 +0530

    setup some basic structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod error;
+mod parser;
+pub mod request;
+pub mod response;
+pub mod router;
+mod server;
